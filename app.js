abe7a014841547db38b2b2fc546e8fa34a95f61d
Robishankar Denre <75752835+RobishankarDenre@users.noreply.github.com>
2023-05-09 09:48:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+// const num = new Date();
+// console.log(num)
+
+// const a = new
+// console.log(document)
+
+
+
+// function onclickbtn(){
+// document.getElementById("demo").innerHTML = "Hello JavaScript!"
+// }
+
+
+
+function myfunction() {
+    document.getElementById("demo").innerHTML = "5 + 6 =" +(5 + 6);
+}
+
+// function sumFun() {
+//     let a, b, c;
+// a = 10;
+// b = 20;
+// c = a + b;
+//     document.getElementById("calculator").innerHTML = c;
+// }
+// function length() {
+//     let txt = 'A';
+// let len = text.length;
+// document.getElementById(len)
+// }
+
+// document.write(10 + 4)
+
+// window.alert(11 + 22)
+
+// alert("Robi")
+
+// console.log(5 + 6);
+
+// sumFun()
+
+// let person = "John Doe",
+// carName = "Volvo",
+//     price = 200;
+// document.write(person,carName,price)
+
+// You can create a const object:
+// const car = {type:"Fiat", model:"500", color:"white"};
+
+// // You can change a property:
+// car.color = "red";
+
+// // You can add a property:
+// car.owner = "Johnson";
+
+// function myFunction(a, b) {
+//   return a * b;             // Function returns the product of a and b
+// }
+// document.write(myFunction(7,6));
+
+// let text = "We are the so-called \"Vikings\" from the north.";
+// document.write(text.length())
+
+// let a = 'Robi';
+// let b = 'Robi';
+// document.write(a==b)
+
